306d9d8ea0cd7971cf4fc218bb91df1a98ab6954
Kaitlyn <ksharo@stevens.edu>
2018-11-09 18:25:48 -0500

    fixed some css

--- js/application.js
+++ js/application.js
@@ -1,4 +1,9 @@
-var x = document.getElementById("people");
-for(let i = 0; i<x; i++){
-
+var x = document.getElementById("add");
+x.addEventListener("click", function(){
+	console.log("clicked");
+	var l = document.createElement(label);
+	var txt = document.createTextNode("Name: ");
+	l.appendChild(txt);
+	var before = document.getElementById("add");
+	document.body.insertBefore(before,l);
 }
