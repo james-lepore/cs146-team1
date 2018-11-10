a3062c76270942eac54bcde076e48b61f1e03f61
james-lepore <43451533+james-lepore@users.noreply.github.com>
2018-11-10 10:25:14 -0500

    Application Javascript
    Add elements on click

--- js/application.js
+++ js/application.js
@@ -1,9 +1,15 @@
-var b = document.getElementById("add");
-b.addEventListener("click", function(){
+function addPerson(){
 	console.log("clicked");
-	var l = document.createElement(label);
+	var l = document.createElement("p");
 	var txt = document.createTextNode("Name: ");
 	l.appendChild(txt);
-	var before = document.getElementById("add");
-	document.body.insertBefore(before,l);
-});
+
+	var p = document.getElementById("placeholder");
+	var d = document.getElementById("placeholderdiv");
+	d.insertBefore(l,p);
+}
+
+/*
+var b = document.getElementById("add");
+b.addEventListener("click", addPerson);
+*/
