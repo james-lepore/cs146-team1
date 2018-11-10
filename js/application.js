42f12f4929c7d07b206c84b779e23b4b3e300343
Kaitlyn <ksharo@stevens.edu>
2018-11-10 09:41:26 -0500

    Worked on my code. didn't help anything :(

--- js/application.js
+++ js/application.js
@@ -1,9 +1,9 @@
-var x = document.getElementById("add");
-x.addEventListener("click", function(){
+var b = document.getElementById("add");
+b.addEventListener("click", function(){
 	console.log("clicked");
 	var l = document.createElement(label);
 	var txt = document.createTextNode("Name: ");
 	l.appendChild(txt);
 	var before = document.getElementById("add");
 	document.body.insertBefore(before,l);
-}
+});
