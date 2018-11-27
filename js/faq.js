2726c811e9c1a44a89898d5aa94cb300aed6a97c
james-lepore <43451533+james-lepore@users.noreply.github.com>
2018-11-27 14:50:11 -0500

    Fixed FAQ JS
    Added Event LIsteners

--- js/faq.js
+++ js/faq.js
@@ -1,34 +1,64 @@
-function showAnswer(num) {
-    var x = document.getElementById("answer" + num);
-    if (x.style.display != "block") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+// function showAnswer(num) {
+//     var x = document.getElementById("answer" + num);
+//     if (x.style.display != "block") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
 
-    var y = document.getElementById("arrow" + num);
-    if (y.style.borderTop != "none"){
-    	y.style.borderBottom = "0.4em solid";
-    	y.style.borderTop = "none";
-    } else{
-    	y.style.borderTop = "0.4em solid";
-    	y.style.borderBottom = "none";
-    }
-}
+//     var y = document.getElementById("arrow" + num);
+//     if (y.style.borderTop != "none"){
+//         y.style.borderBottom = "0.4em solid";
+//         y.style.borderTop = "none";
+//     } else{
+//         y.style.borderTop = "0.4em solid";
+//         y.style.borderBottom = "none";
+//     }
+// }
 
-function convert(){
-    usd = document.getElementById('usd')
-    shc = document.getElementById('shc')
+window.addEventListener('load', function(){
+    questions = document.getElementsByClassName('question');
+    console.log(questions);
 
-    if (usd.value != ''){
-        shc.value = (usd.value / 1000000)
-    }
-    else if (shc.value != ''){
-        usd.value = (shc.value * 1000000)
-    }
-}
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', function(){
+        num = String(i);
+        var x = document.getElementById("answer" + num);
+        if (x.style.display != "block") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
 
-function resetConverter(){
-    document.getElementById('usd').value = ''
-    document.getElementById('shc').value = ''
-}
+        var y = document.getElementById("arrow" + num);
+        if (y.style.borderTop != "none"){
+            y.style.borderBottom = "0.4em solid";
+            y.style.borderTop = "none";
+        } else{
+            y.style.borderTop = "0.4em solid";
+            y.style.borderBottom = "none";
+        }
+    }) 
+    }
+    
+
+
+    convert_button = document.getElementById("convertButton");
+    convert_button.addEventListener('click', function(){
+        usd = document.getElementById('usd')
+        shc = document.getElementById('shc')
+
+        if (usd.value != ''){
+            shc.value = (usd.value / 1000000)
+        }
+        else if (shc.value != ''){
+            usd.value = (shc.value * 1000000)
+        }
+    }); 
+
+    reset_button = document.getElementById("resetConverter");
+    reset_button.addEventListener('click', function(){
+        document.getElementById('usd').value = ''
+        document.getElementById('shc').value = ''
+    });
+});
