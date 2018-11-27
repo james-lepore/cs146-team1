98bd747169417a721fb79346d1a2280a9bb94f5f
james-lepore <43451533+james-lepore@users.noreply.github.com>
2018-11-27 14:34:22 -0500

    Added Event Listeers to main javascript
    works across all pages

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,24 +1,28 @@
-  function topNav() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-          x.className += " responsive";
-      } else {
-          x.className = "topnav";
-      }
+window.addEventListener('load', function(){
+	icon = document.getElementById('icon');
+	icon.addEventListener('click', function(){
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+		  x.className += " responsive";
+		} else {
+		  x.className = "topnav";
+		}
 
-      var y = document.getElementById('container');
-      y.classList.toggle("change");
-  }
-
-function footerSubmit(){
-  var x = document.getElementById("footerEmail");
-  console.log(x.value)
-  if ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x.value))){
-    x.value = '';
-    x.placeholder = 'Success!';
-  }
-  else{
-    x.value = '';
-    x.placeholder = "Invalid, Try again";
-  }
-}
+		var y = document.getElementById('container');
+		y.classList.toggle("change");
+	});
+	  
+	submit_button = document.getElementById("newsletterSubmit");
+	submit_button.addEventListener('click', function(){
+		var x = document.getElementById("footerEmail");
+	  console.log(x.value)
+	  if ((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x.value))){
+	    x.value = '';
+	    x.placeholder = 'Success!';
+	  }
+	  else{
+	    x.value = '';
+	    x.placeholder = "Invalid, Try again";
+	  }
+	});
+});
