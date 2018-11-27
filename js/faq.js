8cbc48dbb7f00dda4e7ddac343ca24690d3e9e50
james-lepore <43451533+james-lepore@users.noreply.github.com>
2018-11-27 15:09:03 -0500

    Update faq.js
    Cleaned up unnecessary code

--- js/faq.js
+++ js/faq.js
@@ -1,47 +1,27 @@
-// function showAnswer(num) {
-//     var x = document.getElementById("answer" + num);
-//     if (x.style.display != "block") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-
-//     var y = document.getElementById("arrow" + num);
-//     if (y.style.borderTop != "none"){
-//         y.style.borderBottom = "0.4em solid";
-//         y.style.borderTop = "none";
-//     } else{
-//         y.style.borderTop = "0.4em solid";
-//         y.style.borderBottom = "none";
-//     }
-// }
-
 window.addEventListener('load', function(){
     questions = document.getElementsByClassName('question');
-    console.log(questions);
+    var num_of_questions = questions.length
 
-    for(let i = 0; i < questions.length; i++){
+    for(let i = 0; i < num_of_questions; i++){
         questions[i].addEventListener('click', function(){
-        num = String(i);
-        var x = document.getElementById("answer" + num);
-        if (x.style.display != "block") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
+	        num = String(i);
+	        var x = document.getElementById("answer" + num);
+	        if (x.style.display != "block") {
+	            x.style.display = "block";
+	        } else {
+	            x.style.display = "none";
+	        }
 
-        var y = document.getElementById("arrow" + num);
-        if (y.style.borderTop != "none"){
-            y.style.borderBottom = "0.4em solid";
-            y.style.borderTop = "none";
-        } else{
-            y.style.borderTop = "0.4em solid";
-            y.style.borderBottom = "none";
-        }
-    }) 
+	        var y = document.getElementById("arrow" + num);
+	        if (y.style.borderTop != "none"){
+	            y.style.borderBottom = "0.4em solid";
+	            y.style.borderTop = "none";
+	        } else{
+	            y.style.borderTop = "0.4em solid";
+	            y.style.borderBottom = "none";
+	        }
+	    });
     }
-    
-
 
     convert_button = document.getElementById("convertButton");
     convert_button.addEventListener('click', function(){
