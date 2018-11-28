57da1a3ba79bae61ac3d058ef2bc4ca6d2a9a7cb
Kaitlyn <ksharo@stevens.edu>
2018-11-28 16:16:55 -0500

    tried some stuff...didn't work :(

--- js/verify.js
+++ js/verify.js
@@ -9,4 +9,18 @@ window.addEventListener('load',function(){
 			labels[i].innerHTML+=values[i+1];
 		}
 	}
+	var verify = document.getElementById('finished');
+	if (verify!=null){
+		verify.addEventListener('click', function(){
+			alert("Your application has been submitted successfuly. Returning to the home page...");
+			window.location='../index.html';
+		});
+	}
+	var fix = document.getElementById('fix');
+	if (fix!=null){
+		fix.addEventListener('click', function(){
+			alert("Found something wrong with your application? Or maybe you're experiencing some last minute nerves. No problem! Returning you to the application page...");
+			window.location='application.html';
+		});
+	}
 });
