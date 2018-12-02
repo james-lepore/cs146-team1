eafd02cf6240b3ce6030aa065b32a2b74ed4e6b7
james-lepore <43451533+james-lepore@users.noreply.github.com>
2018-12-02 13:43:47 -0500

    Deleted Address Line 2 if blank

--- js/verify.js
+++ js/verify.js
@@ -3,11 +3,14 @@ window.addEventListener('load',function(){
 	values = lst.split(',');
 	document.getElementById("vName").innerHTML+=values[0]+' '+values[1];
 	labels = document.getElementsByClassName("vLabels");
-	console.log(values);
-	console.log(labels);
 	for (let i = 1; i < labels.length; i++){
 		if (labels[i]!=null){
-			labels[i].innerHTML+=values[i+1];
+			if(values[i+1] != ''){
+				labels[i].innerHTML+=values[i+1];
+			} else{
+				labels[i].innerHTML = '';
+			}
+			
 		}
 	}
 	var verify = document.getElementById('finished');
